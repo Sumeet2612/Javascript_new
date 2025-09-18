baa5ed1dd3011fb06011cd05f721015c11f94074
Sumeet Kumar Raj <134926910+Sumeet2612@users.noreply.github.com>
2025-09-18 11:49:14 +0000

    container config done

---
+++ 01_basics/01-variables.js
@@ -0,0 +1,20 @@
+const account_Id = 144453
+let accountEmail = "summeetkumarrraj2612@gmail.com"
+var accountPassword = "12345"
+accountCity = "Bengaluru"
+let accountState;
+
+// accountId = 2 // not allowed 
+
+accountEmail = "skr@skr.com"
+accountPassword = "26788982"
+accountCity = "Rourkela"
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope 
+*/
+
+console.table([account_Id , accountEmail , accountPassword , accountCity , accountState])
+
+ 
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+ "use strict"; // treat all JS code as newer version 
+
+// alert (3 + 3) // we are using nodejs , not browser 
+
+console.log(3+3) // code readability shoud be high 
+console.log("Sumeet")
+
+let name = "sumeet"
+let age = 22
+let isLoggedIn = false
+let state = null
+
+//number => 2^53
+//bigint
+
+// string => ""
+// boolean => true/false
+// null  => standalopne value 
+// undefined =>
+//symbol => unique 
+//
