73100e0362b83a047c434294c70a9ef3cfceb054
Sumeet Kumar Raj <134926910+Sumeet2612@users.noreply.github.com>
2025-09-20 11:00:02 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score = "33abc"
+
+/* console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)  // => valueInNumber score = 33abc  => NaN
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 2
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn); */
+
+// 1 => true; 0 => false
+// "" => false 
+// "Sumeet" => true  
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
