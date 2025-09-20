5683c6472a16b7c481e56388f95232e2f3f8db63
Sumeet Kumar Raj <134926910+Sumeet2612@users.noreply.github.com>
2025-09-20 14:49:06 +0000

    Conversion Operation - 2

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -16,7 +16,26 @@ console.log(booleanIsLoggedIn); */
 // "" => false 
 // "Sumeet" => true  
 
-let someNumber = 33
+/*let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
-console.log(typeof stringNumber)
+console.log(typeof stringNumber) */
+
+
+// ************Operations**************
+
+let value = 3
+let negValue = -value 
+console.log(negValue);
+
+let str1 = "hello"
+let str2 = " sumeet"
+
+let str3 = str1 + str2
+console.log(str3);
+
+/*console.log("1" + 2);
+console.log(1+"2")
+console.log("1" + 2 + 2)*/
+
+console.log(true);
