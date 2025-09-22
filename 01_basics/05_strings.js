15265023b2ef254885746ca2167f400b7578bb10
Sumeet Kumar Raj <134926910+Sumeet2612@users.noreply.github.com>
2025-09-22 13:38:29 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,28 @@
+const name = "Sumeet"
+const repoCount = 4
+
+// console.log(name + repoCount + " value");
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); 
+// String Interpolation modern methods 
+
+const gamename = new String('Sumeets-shc')
+
+console.log(gamename[0]);
+// console.log(gamename.__proto__);
+
+// console.log(gamename.length);
+// console.log(gamename.toUpperCase());
+// console.log(gamename.charAt(5));
+console.log(gamename.indexOf('t'));
+
+// const newString = gamename.substring(0 , 4)
+// console.log(newString);
+
+
+// const anotherString = gamename.slice(-10 , 4)
+// console.log(anotherString);
+
+const newStringOne = "   Sumeet   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
