b7c9e36490100fbba7ffbdcbb05d8b58231e77e0
Sumeet Kumar Raj <134926910+Sumeet2612@users.noreply.github.com>
2025-09-23 14:06:06 +0000

    Maths operations

--- 01_basics/06_nums_maths.js
+++ 01_basics/06_nums_maths.js
@@ -25,12 +25,12 @@ console.log(Math.floor(4.9));
 console.log(Math.min(4 , 7 ,8 ,9))
 console.log(Math.max(4 , 7 ,8 ,9)); */
 
-console.log(Math.floor(Math.random()*10 ));
+console.log(Math.floor(Math.random()*10));
 
 const min = 10
 const max = 20;
 
-Math.random()
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
 
 
 
