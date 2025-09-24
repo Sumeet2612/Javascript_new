dbd7c0fdb9953d777508689994ecd308995f32f2
Sumeet Kumar Raj <134926910+Sumeet2612@users.noreply.github.com>
2025-09-24 14:59:14 +0000

    Arrays part 2

--- 02_basics/arrays.js
+++ 02_basics/arrays.js
@@ -38,6 +38,8 @@ const myn1 = myArr.slice(1,3)
 console.log(myn1);
 console.log("B ",myArr);
 
+
+
 const myn2 = myArr.splice(1,3) // manipulates the original array 
 console.log("C " , myArr);
 console.log(myn2);
