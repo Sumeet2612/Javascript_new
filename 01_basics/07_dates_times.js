b1dc4daa2eec2b905e0f6854efcab51b93cb2b64
Sumeet Kumar Raj <134926910+Sumeet2612@users.noreply.github.com>
2025-09-24 13:29:36 +0000

    Date and Time

--- 01_basics/07_dates_times.js
+++ 01_basics/07_dates_times.js
@@ -1,6 +1,33 @@
 // Dates 
 
 let myDate = new Date()
-console.log(myDate);
+// console.log(myDate);
+// console.log(myDate.toDateString());
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
 
 
+// let myCreatedDate = new Date(2023, 0, 23)
+ let myCreatedDate = new Date(2023, 11, 23 , 5, 3)
+// let myCreatedDate = new Date("2023-10-14")
+// let myCreatedDate = new Date("12-10-2025")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+// console.log(newDate.getDate());
+// console.log(newDate.getDay());
+// console.log(newDate.getMonth());
+
+// `${newDate.getDay()} and the time `
+
+newDate.toLocaleString('default', {
+    weekday : "long",
+})
