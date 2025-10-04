5514035ad1fa2b8e6fea7aa2d85a64597fdea99f
Sumeet Kumar Raj <134926910+Sumeet2612@users.noreply.github.com>
2025-10-04 12:45:15 +0000

    Objects Part 1

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,39 @@
+// Objects can be declared as literal and constructor 
+
+/* singelton --> When we declare objects through literals then the object create 
+can't be declared as singelton but it can be done by constructor */
+
+// object literals
+// Object.create constructor way of introducing objects 
+
+const mySym = Symbol("key1")
+
+const Jsuser = { 
+    name : "Sumeet",
+    age : 22 ,
+    location : "Bangalore",
+    [mySym] : "mykey1",
+    email: "sumeetkumarraj2612@gmail.com",
+    isLoggedIn : false,
+    lastLogin : ["Monday" , "Tuesday"]
+}
+
+// console.log(Jsuser.email);
+// console.log(Jsuser["email"]);
+// console.log(typeof Jsuser["mySym"]);
+
+Jsuser.email = "sumeet@gmail.com"
+// Object.freeze(Jsuser)
+Jsuser.email = "hitesh@chatgpt.com"
+// console.log(Jsuser);
+
+Jsuser.greeting = function () {
+    console.log("Hello User");
+}
+
+Jsuser.greetingTwo = function () {
+    console.log(`Hello Js user, ${this.name}`);
+}
+
+console.log(Jsuser.greeting());
+console.log(Jsuser.greetingTwo());
