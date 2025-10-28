700eb51ff3cc91f2c569bed565982f34376a43a7
Sumeet Kumar Raj <134926910+Sumeet2612@users.noreply.github.com>
2025-10-28 15:12:54 +0000

    Revison of arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -11,37 +11,41 @@ const myArr2 = new Array(1,2,3,4)
 
 // Array methods
 
-// myArr.push(6)
-// myArr.push(7)
+myArr.push(6)
+myArr.push(7)
 // myArr.pop() // just pops out the last element 
 
+// console.log(myArr);
+
 // myArr.unshift(9)
+
+// console.log(myArr);
 // myArr.shift()
-
+// console.log(myArr);
 // console.log(myArr.includes(3));
-// console.log(myArr.indexOf(3)); 
+// console.log(myArr.indexOf(10)); 
 // console.log(myArr);
 
-// const newArr = myArr.join()
+const newArr = myArr.join()
 
-// console.log(myArr);
-// console.log(newArr);
+console.log(myArr);
+console.log(newArr);
 
 // console.log(typeof newArr);
 
 // slice , splice 
 
-console.log("A" , myArr);
+// console.log("A" , myArr);
 
 const myn1 = myArr.slice(1,3)
-console.log(myn1);
-console.log("B ",myArr);
+// console.log(myn1);
+// console.log("B ",myArr);
 
 
 
  const myn2 = myArr.splice(1,3) // manipulates the original array 
- console.log("C ",myArr);
- console.log(myn2);
+//  console.log("C ",myArr);
+//  console.log(myn2);
 
 
 
