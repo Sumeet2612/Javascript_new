6532fbc14b3ae933b75682d71a644d1f8b4b8e79
Sumeet Kumar Raj <134926910+Sumeet2612@users.noreply.github.com>
2026-01-12 10:54:28 +0000

    Revision of Arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -3,16 +3,17 @@
 const myArr = [0 , 1 , 2 , 3 , 4 , 5]
 const myHeroes = ["Superman" , "Batman"]
 
-const myArr2 = new Array(1,2,3,4)
+const myArr2 = new Array(1,2,3,4) // defining a new array 
 
 // console.log(myArr[0]);
 // console.log(myArr2);
 // console.log(myHeroes);
 
 // Array methods
-
-myArr.push(6)
-myArr.push(7)
+/* JavaScript arrays are not associative arrays and so, array elements cannot 
+be accessed using arbitrary strings as indexes, but must be accessed using nonnegative integers (or their respective string form) as indexes*/
+// myArr.push(6)
+// myArr.push(7)
 // myArr.pop() // just pops out the last element 
 
 // console.log(myArr);
@@ -20,32 +21,35 @@ myArr.push(7)
 // myArr.unshift(9)
 
 // console.log(myArr);
-// myArr.shift()
+// console.log(myArr2)
+myArr.shift() // removes the first element of the array 
 // console.log(myArr);
 // console.log(myArr.includes(3));
 // console.log(myArr.indexOf(10)); 
 // console.log(myArr);
 
-const newArr = myArr.join()
+const newArr = myArr.join()  /*The join() method of Array instances creates and returns a 
+new string by concatenating all of the elements in this array, separated by commas or a specified separator string */
 
-console.log(myArr);
-console.log(newArr);
+// console.log(myArr);
+// console.log(newArr);
 
-// console.log(typeof newArr);
+// console.log(typeof newArr); 
 
 // slice , splice 
 
 // console.log("A" , myArr);
 
-const myn1 = myArr.slice(1,3)
-// console.log(myn1);
-// console.log("B ",myArr);
+const myn1 = myArr.slice(1,3) /* he slice() method of Array instances returns a shallow copy of a portion of an array into a new array 
+object selected from start to end (end not included) where start and end represent the index of items in that array. */
+console.log(myn1);
+console.log("B ",myArr);
 
 
 
- const myn2 = myArr.splice(1,3) // manipulates the original array 
-//  console.log("C ",myArr);
-//  console.log(myn2);
+const myn2 = myArr.splice(1,3) // manipulates the original array 
+console.log("C ",myArr);
+console.log(myn2);
 
 
 
